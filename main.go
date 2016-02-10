80f58bca61aa1c73ae46c695395eaf4d2f510f17
Derek P Sifford <dereksifford@gmail.com>
2016-02-10 00:59:05 -0500

    Initial working version + CSV overhaul. Closes #1

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/tealeg/xlsx"
+	"encoding/csv"
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -12,21 +12,47 @@ import (
 )
 
 type space struct {
-	Name, Status, Updated string
-	Available             int
+	Name, Status, Updated, Available string
 }
 
-type structure []space
+type structure struct {
+	Name, URLCode string
+	Number        int
+	Spaces        []space
+}
 
 func main() {
-	data := getData()
-	file := openFile()
-	saveData(file, data)
+
+	c := make(chan structure)
+	go getStructures(c)
+	for i := range c {
+		writeData(i)
+	}
+
 }
 
-func getData() structure {
+func getStructures(c chan<- structure) {
+	for i := 1; i < 9; i++ {
+		if i == 7 || i == 4 {
+			continue
+		}
 
-	structure6 := structure{
+		s := structure{
+			Name:    "Structure" + strconv.Itoa(i),
+			Number:  i,
+			URLCode: strconv.Itoa(i + 88),
+		}
+		s.Spaces = getSpaces(s)
+
+		c <- s
+	}
+	close(c)
+}
+
+// TODO: Turn this into an interface method
+func getSpaces(s structure) []space {
+
+	spaces := []space{
 		space{Name: "WSU Permit"},
 		space{Name: "Student OneCard"},
 		space{Name: "Visitor"},
@@ -37,29 +63,24 @@ func getData() structure {
 		"updated": regexp.MustCompile(`(?P<1>^.+: )(?P<2>.+)`),
 	}
 
-	// TODO: Implement this for all structures.
-	// Parking structure URL "location"s are numbered starting from 89
-	// Thus...
-	// 1 = 89, 2 = 90, 3 = 91, 4 = 92, 5 = 93, 6 = 94, 8 = 96
-
 	// Request
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://m.wayne.edu/parking.php?location=94", nil)
+	req, err := http.NewRequest("GET", "http://m.wayne.edu/parking.php?location="+s.URLCode, nil)
 	if err != nil {
-		panic(err)
+		log.Fatalln("Error making request:", err)
 	}
 	req.Header.Set("User-Agent", "Apple-iPhone6C1/")
 
 	// Response
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatalln("Error sending request:", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := html.Parse(resp.Body)
 	if err != nil {
-		panic(err)
+		log.Fatalln("Error parsing response body:", err)
 	}
 
 	// Parse relevant response data
@@ -67,79 +88,39 @@ func getData() structure {
 	lastUpdated, _ := scrape.Find(body, scrape.ByClass("last_updated"))
 
 	avail := re["avail"].FindAllString(scrape.Text(dataString), -1)
+	if len(avail) == 0 {
+		avail = []string{"0", "0", "0"}
+	}
 	status := re["status"].FindAllString(scrape.Text(dataString), -1)
 	updated := re["updated"].FindStringSubmatch(scrape.Text(lastUpdated))[2]
 
-	fmt.Println(updated)
-
-	for key := range structure6 {
-		structure6[key].Available, _ = strconv.Atoi(avail[key])
-		structure6[key].Status = status[key]
-		structure6[key].Updated = updated
+	for key := range spaces {
+		spaces[key].Available = avail[key]
+		spaces[key].Status = status[key]
+		spaces[key].Updated = updated
 	}
 
-	return structure6
+	return spaces
 
 }
 
-func openFile() (file *xlsx.File) {
+func writeData(s structure) {
 
-	// TODO: Pick an actual directory for this to run in (it's going to run as a cron task)
-
-	if _, err := os.Stat("/home/dsifford/Dropbox/ParkingData/data.xlsx"); os.IsNotExist(err) {
-
-		file := xlsx.NewFile()
-		sheet, err := file.AddSheet("Structure 6")
-		if err != nil {
-			panic(err)
-		}
-
-		headingStyle := xlsx.NewStyle()
-		headingStyle.Font.Bold = true
-
-		row := sheet.AddRow()
-		for _, name := range []string{"Updated", "Type", "Status", "Spaces Available"} {
-			cell := row.AddCell()
-			cell.SetStyle(headingStyle)
-			cell.SetString(name)
-		}
-
-		return file
-
-	}
-
-	file, err := xlsx.OpenFile("/home/dsifford/Dropbox/ParkingData/data.xlsx")
+	file, err := os.OpenFile(s.Name+".csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		panic(err)
+		log.Fatalln("CSV file could not be created or opened:", err)
 	}
-	return file
+	defer file.Close()
 
-}
+	writer := csv.NewWriter(file)
 
-func saveData(f *xlsx.File, s structure) {
-
-	// FIXME: Fix dirty code below
-	for _, sheet := range f.Sheets {
-		for _, spaceType := range s {
-
-			row := sheet.AddRow()
-
-			cell := row.AddCell()
-			cell.SetString(spaceType.Updated)
-
-			cell = row.AddCell()
-			cell.SetString(spaceType.Name)
-
-			cell = row.AddCell()
-			cell.SetString(spaceType.Status)
-
-			cell = row.AddCell()
-			cell.SetInt(spaceType.Available)
-		}
+	if stat, _ := file.Stat(); stat.Size() == 0 {
+		writer.Write([]string{"Updated", "Type", "Status", "Spaces Available"})
 	}
 
-	if err := f.Save("/home/dsifford/Dropbox/ParkingData/data.xlsx"); err != nil {
-		panic(err)
+	for _, sp := range s.Spaces {
+		writer.Write([]string{sp.Updated, sp.Name, sp.Status, sp.Available})
 	}
+	writer.Flush()
 
 }
